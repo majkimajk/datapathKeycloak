0cf93cc10a0fa2cd4b1d8eca22992aa0ca09fccd
MichalB <mbaran@bluesoft.net.pl>
2019-05-27 17:48:06 +0200

    refactored js code to angular + added authorization

--- src/main/java/org/keycloak/datapath/rest/LongAttributesRestResource.java
+++ src/main/java/org/keycloak/datapath/rest/LongAttributesRestResource.java
@@ -3,9 +3,20 @@ package org.keycloak.datapath.rest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jboss.logging.Logger;
+import org.jboss.resteasy.spi.NotFoundException;
+import org.keycloak.common.ClientConnection;
 import org.keycloak.datapath.model.LongAttributesMapping;
 import org.keycloak.datapath.spi.LongAttributeService;
+import org.keycloak.jose.jws.JWSInput;
+import org.keycloak.jose.jws.JWSInputException;
+import org.keycloak.models.ClientModel;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.representations.AccessToken;
+import org.keycloak.services.managers.AppAuthManager;
+import org.keycloak.services.managers.AuthenticationManager;
+import org.keycloak.services.managers.RealmManager;
+import org.keycloak.services.resources.admin.AdminAuth;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
@@ -24,9 +35,15 @@ public class LongAttributesRestResource {
 
     private final KeycloakSession session;
 
+    protected AppAuthManager authManager;
+
     public LongAttributesRestResource(KeycloakSession session) {
         this.session = session;
+        this.authManager = new AppAuthManager();
     }
+
+    @Context
+    protected ClientConnection clientConnection;
 
 
     @GET
@@ -34,39 +51,98 @@ public class LongAttributesRestResource {
     public Response getLongAttributes(@PathParam("userId") String userId, @Context UriInfo uriInfo,
                                       @Context HttpServletRequest httpServletRequest,
                                       @Context HttpHeaders headers) throws JsonProcessingException {
-        final LongAttributeService attributeService = this.session.getProvider(LongAttributeService.class);
-        List<LongAttributesMapping> attributesMappings = attributeService.getAttributeList(userId);
-        return Response.ok(MAPPER.writeValueAsBytes(attributesMappings)).type(MediaType.APPLICATION_JSON_TYPE).build();
+        try {
+            AdminAuth auth = authenticateRealmAdminRequest(headers);
+            final LongAttributeService attributeService = this.session.getProvider(LongAttributeService.class);
+            List<LongAttributesMapping> attributesMappings = attributeService.getAttributeList(userId);
+            return Response.ok(MAPPER.writeValueAsBytes(attributesMappings)).type(MediaType.APPLICATION_JSON_TYPE).build();
+        } catch (Exception e) {
+            return badResponse(e);
+        }
     }
 
     @POST
     @Path("{userId}/longAttributes")
-    public Response addOrUpdateLongAttributes(@PathParam("userId") String userId, List<LongAttributesMapping> attributes) throws JsonProcessingException {
-
-        final LongAttributeService attributeService = this.session.getProvider(LongAttributeService.class);
-        attributeService.addAttributes(attributes, userId);
-        return okResponse();
+    public Response addOrUpdateLongAttributes(@Context HttpHeaders headers, @PathParam("userId") String userId, List<LongAttributesMapping> attributes) throws JsonProcessingException {
+       try {
+           AdminAuth auth = authenticateRealmAdminRequest(headers);
+           final LongAttributeService attributeService = this.session.getProvider(LongAttributeService.class);
+           attributeService.addAttributes(attributes, userId);
+           return okResponse();
+       } catch (Exception e) {
+           return badResponse(e);
+       }
     }
 
     @DELETE
-    @Path("{userId}/longAttributes")
-    public Response deleteLongAttribute(@PathParam("userId") String userId, LongAttributesMapping attribute) throws JsonProcessingException {
-        final LongAttributeService attributeService = this.session.getProvider(LongAttributeService.class);
-        attributeService.deleteAttribute(attribute, userId);
-        return okResponse();
+    @Path("{userId}/longAttrixbutes")
+    public Response deleteLongAttribute(@Context HttpHeaders headers, @PathParam("userId") String userId, LongAttributesMapping attribute) throws JsonProcessingException {
+        try {
+            AdminAuth auth = authenticateRealmAdminRequest(headers);
+            LOGGER.debug("authenticated admin access for: " + auth.getUser().getUsername());
+            final LongAttributeService attributeService = this.session.getProvider(LongAttributeService.class);
+            attributeService.deleteAttribute(attribute, userId);
+            return okResponse();
+        } catch (Exception e) {
+            return badResponse(e);
+        }
     }
 
-    @POST
-    @Path("{userId}/update/longAttributes")
-    public Response updateLongAttributes(@PathParam("userId") String userId, List<LongAttributesMapping> attributes) throws JsonProcessingException {
+    @PUT
+    @Path("{userId}/longAttributes")
+    public Response updateLongAttributes(@Context HttpHeaders headers, @PathParam("userId") String userId, List<LongAttributesMapping> attributes) throws JsonProcessingException {
+        try {
+            AdminAuth auth = authenticateRealmAdminRequest(headers);
+            LOGGER.debug("authenticated admin access for: " + auth.getUser().getUsername());
+            //final String accessToken = headers.getHeaderString("Authorization");
+            final LongAttributeService attributeService = this.session.getProvider(LongAttributeService.class);
+            attributeService.updateAttributes(attributes, userId);
+            return okResponse();
 
-        final LongAttributeService attributeService = this.session.getProvider(LongAttributeService.class);
-        attributeService.updateAttributes(attributes, userId);
-        return okResponse();
+        } catch (Exception e) {
+            return badResponse(e);
+        }
+
     }
 
 
     private Response okResponse() throws JsonProcessingException {
         return Response.ok(MAPPER.writeValueAsBytes("OK")).type(MediaType.APPLICATION_JSON_TYPE).build();
+    }
+
+    private Response badResponse(Exception e) throws JsonProcessingException {
+        return Response.ok(MAPPER.writeValueAsBytes(e.getCause())).type(MediaType.APPLICATION_JSON_TYPE).build();
+    }
+
+    protected AdminAuth authenticateRealmAdminRequest(HttpHeaders headers) {
+        String tokenString = authManager.extractAuthorizationHeaderToken(headers);
+        if (tokenString == null) throw new NotAuthorizedException("Bearer");
+        AccessToken token;
+        try {
+            JWSInput input = new JWSInput(tokenString);
+            token = input.readJsonContent(AccessToken.class);
+        } catch (JWSInputException e) {
+            throw new NotAuthorizedException("Bearer token format error");
+        }
+        String realmName = token.getIssuer().substring(token.getIssuer().lastIndexOf('/') + 1);
+        RealmManager realmManager = new RealmManager(session);
+        RealmModel realm = realmManager.getRealmByName(realmName);
+        if (realm == null) {
+            throw new NotAuthorizedException("Unknown realm in token");
+        }
+        session.getContext().setRealm(realm);
+        AuthenticationManager.AuthResult authResult = authManager.authenticateBearerToken(session, realm, session.getContext().getUri(), clientConnection, headers);
+        if (authResult == null) {
+            LOGGER.debug("Token not valid");
+            throw new NotAuthorizedException("Bearer");
+        }
+
+        ClientModel client = realm.getClientByClientId(token.getIssuedFor());
+        if (client == null) {
+            throw new ClientErrorException("Could not find client for authorization", Response.Status.NOT_FOUND);
+
+        }
+
+        return new AdminAuth(realm, authResult.getToken(), authResult.getUser(), client);
     }
 }
--- src/main/resources/theme/longerattributes/admin/resources/js/longAttributes.js
+++ src/main/resources/theme/longerattributes/admin/resources/js/longAttributes.js
@@ -24,168 +24,45 @@ module.controller('LongerAttributesCtrl', function($scope, realm, user, BruteFor
                                              UserStorageOperations,
                                              $location, $http, Dialog, Notifications) {
     $scope.realm = realm;
-    $scope.create = !user.id;
-    $scope.editUsername = $scope.create || $scope.realm.editUsernameAllowed;
 
 
-    if ($scope.create) {
-        $scope.user = { enabled: true, attributes: {} }
-    } else {
-        if (!user.attributes) {
-            user.attributes = {}
-        }
-        convertAttributeValuesToString(user);
+	$scope.init =  function() {
 
-        addLongerAttributes();
+        var longAttrUrl = authUrl + "/realms/" + realm.id + "/um-authz/" + user.id + "/longAttributes";
 
-        $scope.user = angular.copy(user);
-        $scope.impersonate = function() {
-            UserImpersonation.save({realm : realm.realm, user: $scope.user.id}, function (data) {
-                if (data.sameRealm) {
-                    window.location = data.redirect;
-                } else {
-                    window.open(data.redirect, "_blank");
-                }
-            });
-        };
-        if(user.federationLink) {
-            console.log("federationLink is not null. It is " + user.federationLink);
+		$http.get(longAttrUrl).then(function(response) {
+            responseData = response.data;
+			var lAttributesFromDB = convertAttributeValuesToMap(responseData)
+			$scope.lAttr = angular.copy(lAttributesFromDB);
+			$scope.$watch('lAttr', function() {
+				if (!angular.equals($scope.lAttr, lAttributesFromDB)) {
+					$scope.changed = true;
+				}
+			}, true);
+        });
 
-            if ($scope.access.viewRealm) {
-                Components.get({realm: realm.realm, componentId: user.federationLink}, function (link) {
-                    $scope.federationLinkName = link.name;
-                    $scope.federationLink = "#/realms/" + realm.realm + "/user-storage/providers/" + link.providerId + "/" + link.id;
-                });
-            } else {
-                // KEYCLOAK-4328
-                UserStorageOperations.simpleName.get({realm: realm.realm, componentId: user.federationLink}, function (link) {
-                    $scope.federationLinkName = link.name;
-                    $scope.federationLink = $location.absUrl();
-                })
-            }
+    };
+	$scope.user = angular.copy(user);
 
-        } else {
-            console.log("federationLink is null");
-        }
-        if(user.origin) {
-            if ($scope.access.viewRealm) {
-                Components.get({realm: realm.realm, componentId: user.origin}, function (link) {
-                    $scope.originName = link.name;
-                    $scope.originLink = "#/realms/" + realm.realm + "/user-storage/providers/" + link.providerId + "/" + link.id;
-                })
-            }
-            else {
-                // KEYCLOAK-4328
-                UserStorageOperations.simpleName.get({realm: realm.realm, componentId: user.origin}, function (link) {
-                    $scope.originName = link.name;
-                    $scope.originLink = $location.absUrl();
-                })
-             }
-        } else {
-            console.log("origin is null");
-        }
-        console.log('realm brute force? ' + realm.bruteForceProtected)
-        $scope.temporarilyDisabled = false;
-        var isDisabled = function () {
-            BruteForceUser.get({realm: realm.realm, userId: user.id}, function(data) {
-                console.log('here in isDisabled ' + data.disabled);
-                $scope.temporarilyDisabled = data.disabled;
-            });
-        };
 
-        console.log("check if disabled");
-        isDisabled();
-
-        $scope.unlockUser = function() {
-            BruteForceUser.delete({realm: realm.realm, userId: user.id}, function(data) {
-                isDisabled();
-            });
-        }
-    }
-
-    $scope.changed = false; // $scope.create;
-    if (user.requiredActions) {
-        for (var i = 0; i < user.requiredActions.length; i++) {
-            console.log("user require action: " + user.requiredActions[i]);
-        }
-    }
-    // ID - Name map for required actions. IDs are enum names.
-    RequiredActions.query({realm: realm.realm}, function(data) {
-        $scope.userReqActionList = [];
-        for (var i = 0; i < data.length; i++) {
-            console.log("listed required action: " + data[i].name);
-            if (data[i].enabled) {
-                var item = data[i];
-                $scope.userReqActionList.push(item);
-            }
-        }
-    console.log("---------------------");
-    console.log("ng-model: user.requiredActions=" + JSON.stringify($scope.user.requiredActions));
-    console.log("---------------------");
-    console.log("ng-repeat: userReqActionList=" + JSON.stringify($scope.userReqActionList));
-    console.log("---------------------");
-    });
-    $scope.$watch('user', function() {
-        if (!angular.equals($scope.user, user)) {
-            $scope.changed = true;
-        }
-    }, true);
+	$scope.init();
 
     $scope.save = function() {
-        convertAttributeValuesToLists();
-
-        if ($scope.create) {
-            User.save({
-                realm: realm.realm
-            }, $scope.user, function (data, headers) {
-                $scope.changed = false;
-                convertAttributeValuesToString($scope.user);
-                user = angular.copy($scope.user);
-                var l = headers().location;
-
-                console.debug("Location == " + l);
-
-                var id = l.substring(l.lastIndexOf("/") + 1);
-
-
-                $location.url("/realms/" + realm.realm + "/users/" + id);
-                Notifications.success("The user has been created.");
-            });
-        } else {
-            User.update({
-                realm: realm.realm,
-                userId: $scope.user.id
-            }, $scope.user, function () {
-                $scope.changed = false;
-                convertAttributeValuesToString($scope.user);
-                user = angular.copy($scope.user);
+		var longAttrUrl = authUrl + "/realms/" + "test" + "/um-authz/" + "95d20c88-68a7-4689-88a9-3f447be22011" + "/longAttributes"
+        $http.put(longAttrUrl, convertAttributeValuesToRequest($scope.lAttr)).then(function(response) {
+            var responseData = response.data;
+            if (responseData ===  "OK") {
+				$scope.changed = false;
                 Notifications.success("Your changes have been saved to the user.");
-            });
-        }
+
+            }
+        });
+
     };
 
-    function convertAttributeValuesToLists() {
-        var attrs = $scope.user.attributes;
-        for (var attribute in attrs) {
-            if (typeof attrs[attribute] === "string") {
-                var attrVals = attrs[attribute].split("##");
-                attrs[attribute] = attrVals;
-            }
-        }
-    }
-
-    function convertAttributeValuesToString(user) {
-        var attrs = user.attributes;
-        for (var attribute in attrs) {
-            if (typeof attrs[attribute] === "object") {
-                var attrVals = attrs[attribute].join("##");
-                attrs[attribute] = attrVals;
-            }
-        }
-    }
 
     $scope.reset = function() {
-        $scope.user = angular.copy(user);
+        $scope.lAttr = angular.copy(lAttr);
         $scope.changed = false;
     };
 
@@ -193,147 +70,38 @@ module.controller('LongerAttributesCtrl', function($scope, realm, user, BruteFor
         $location.url("/realms/" + realm.realm + "/users");
     };
 
-    $scope.addAttribute = function() {
-        $scope.user.attributes[$scope.newAttribute.key] = $scope.newAttribute.value;
-        delete $scope.newAttribute;
+    $scope.addLongerAttribute = function() {
+        $scope.lAttr[$scope.newLongerAttribute.key] = $scope.newLongerAttribute.value;
+        delete $scope.newLongerAttribute;
     }
 
-    $scope.removeAttribute = function(key) {
-        delete $scope.user.attributes[key];
+    $scope.removeLongerAttribute = function(key) {
+        delete $scope.lAttr[key];
     }
 
-    $scope.saveCurrentLongerAttributes = function() {
-        var longAttrUrl = authUrl + "/realms/" + "test" + "/um-authz/" + "95d20c88-68a7-4689-88a9-3f447be22011" + "/update/longAttributes"
-        $http.post(longAttrUrl, currentLongerAttributes).then(function(response) {
-            var responseData = response.data;
-            if (responseData ===  "OK") {
-                disableSaveAndResetButtons();
-                Notifications.success("Your changes have been saved to the user.");
-                longerAttributesFromDatabase = JSON.parse(JSON.stringify(currentLongerAttributes));
+    function convertAttributeValuesToRequest(attrs) {
+
+		var attrToPut = [];
+        for (const [key, value] of Object.entries(attrs)) {
+			var newAttribute = {attributeKey:key, attributeValue:value};
+			attrToPut.push(newAttribute);
+        };
+		return attrToPut;
+
+    }
+
+    function convertAttributeValuesToMap(responseData) {
+		var lAttributesFromDB = new Map();
+			for (var lAttribute of responseData) {
+				var key = lAttribute.attributeKey;
+                var value = lAttribute.attributeValue;
+				lAttributesFromDB[key] = value;
+                lAttributesFromDB.set(key, value);
             }
-        });
-    };
-
-
-    function addLongerAttributes() {
-
-        var longAttrUrl = authUrl + "/realms/" + realm.id + "/um-authz/" + user.id + "/longAttributes";
-        $http.get(longAttrUrl).then(function(response) {
-            var responseData = response.data;
-            longerAttributesFromDatabase = responseData;
-            var tbody = document.getElementById('longAttributesTable');
-            for (var lAttribute of responseData) {
-                fillLongerAttributesTable(tbody, lAttribute.attributeKey, lAttribute.attributeValue)
-            }
-        });
+		return lAttributesFromDB;
     }
 
 
 });
 
-function fillLongerAttributesTable(tbody, key, value) {
-     for (var longerAttribute of currentLongerAttributes) {
-            if (longerAttribute.attributeKey === key) {
-                document.getElementById('newLongAttributeKey').value = "";
-                document.getElementById('newLongAttributeValue').value = "";
-                return;
-            }
-    }
 
-    fillCurrentLongerAttributes(key,value);
-
-    var newRow = tbody.insertRow(tbody.children.length);
-    newRow.id = "row-" + key;
-
-    var keyCell = newRow.insertCell(0);
-    var valueCell = newRow.insertCell(1);
-    var funcCell = newRow.insertCell(2);
-
-    var keyValue = document.createTextNode(key);
-    var valueInput = document.createElement("input");
-    var funcValue = document.createTextNode("Delete");
-
-
-    valueInput.setAttribute('type', 'text');
-    valueInput.value = value;
-    valueInput.id = "longAttribute-" + key;
-    valueInput.classList.add("form-control");
-    valueInput.classList.add("ng-pristine");
-    valueInput.classList.add("ng-untouched");
-    valueInput.classList.add("ng-valid");
-    valueInput.classList.add("ng-not-empty");
-    valueInput.onkeyup = function() {fillCurrentLongerAttributes(key, valueInput.value);}
-
-    funcCell.classList.add("kc-action-cell");
-    funcCell.onclick = function() {removeLongAttribute(key);};
-
-    keyCell.appendChild(keyValue);
-    valueCell.appendChild(valueInput);
-    funcCell.appendChild(funcValue);
-
-
-}
-
-function fillCurrentLongerAttributes(key, value) {
-    for (var longerAttribute of currentLongerAttributes) {
-        if (longerAttribute.attributeKey === key) {
-            longerAttribute.attributeValue = value;
-            enableSaveAndResetButtons();
-            return;
-        }
-    }
-    var longerAttributePair = new Object();
-    longerAttributePair.attributeKey = key;
-    longerAttributePair.attributeValue = value;
-    currentLongerAttributes.push(longerAttributePair);
-}
-
-function addLongAttribute() {
-    var newAttrKeyField = document.getElementById('newLongAttributeKey');
-    var newAttrValueField = document.getElementById('newLongAttributeValue');
-    var tbody = document.getElementById('longAttributesTable');
-    fillLongerAttributesTable(tbody, newAttrKeyField.value, newAttrValueField.value);
-    newAttrKeyField.value = "";
-    newAttrValueField.value = "";
-    enableSaveAndResetButtons();
-}
-
-function removeLongAttribute(key) {
-    for (var i = 0; i < currentLongerAttributes.length; i++) {
-        if (currentLongerAttributes[i].attributeKey === key) {
-            currentLongerAttributes.splice(i, 1);
-            var row = document.getElementById("row-" + key);
-            row.parentNode.removeChild(row);
-            enableSaveAndResetButtons()
-            return;
-        }
-    }
-}
-
-
-
-function resetCurrentLongerAttributes() {
-    var tbody = document.getElementById('longAttributesTable');
-    while (tbody.hasChildNodes()) {
-        tbody.removeChild(tbody.firstChild);
-    }
-    currentLongerAttributes = [];
-
-    for (var lAttribute of longerAttributesFromDatabase) {
-        fillLongerAttributesTable(tbody, lAttribute.attributeKey, lAttribute.attributeValue)
-    }
-
-    disableSaveAndResetButtons();
-}
-
-
-function enableSaveAndResetButtons() {
-    document.getElementById("saveLongerAttributes").disabled = false;
-    document.getElementById("resetLongerAttributes").disabled = false;
-}
-
-
-function disableSaveAndResetButtons() {
-    document.getElementById("saveLongerAttributes").disabled = true;
-    document.getElementById("resetLongerAttributes").disabled = true;
-}
